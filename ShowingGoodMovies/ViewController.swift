5253e7dbf4fa94d3fc62b43a19155de3c1e8a650
Mert <mert.ejd@gmail.com>
2021-06-09 00:41:31 +0300

    Initial Commit

---
+++ ShowingGoodMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShowingGoodMovies
+//
+//  Created by Mert Ejder on 9.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
