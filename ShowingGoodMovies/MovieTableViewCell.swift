cb3972c4647e6e995048bc42218cf6e6e21b5eec
Mert <mert.ejd@gmail.com>
2021-06-09 01:00:02 +0300

    added screens

---
+++ ShowingGoodMovies/MovieTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieTableViewCell.swift
+//  ShowingGoodMovies
+//
+//  Created by Mert Ejder on 9.06.2021.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var overview: UILabel!
+    
+    
+}
--- ShowingGoodMovies/ViewControllers/DetailViewController.swift
+++ ShowingGoodMovies/ViewControllers/DetailViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +20,5 @@ class DetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
